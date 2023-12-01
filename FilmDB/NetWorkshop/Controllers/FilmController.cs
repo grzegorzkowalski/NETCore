6b3cce644b0c13e75dc3362eeb66e24a3905c255
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-12-01 11:29:43 +0100

    FilmDB Chapter 5

---
+++ FilmDB/NetWorkshop/Controllers/FilmController.cs
@@ -0,0 +1,27 @@
+﻿using FilmDB.Models;
+using FilmDB.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDB.Controllers
+{
+    public class FilmController : Controller
+    {
+        private FilmManager _manager;
+        public FilmController(FilmManager manager) 
+        { 
+            _manager = manager;
+        }
+        public IActionResult Index()
+        {
+            var film = new Film()
+            {
+                ID = 2,
+                Title = "Uwolnić orkę",
+                Year = 1993
+            };
+
+            _manager.AddFilm(film);
+            return View();
+        }
+    }
+}
---
+++ FilmDB/NetWorkshop/Repositories/FilmManager.cs
@@ -0,0 +1,61 @@
+﻿using FilmDB.Models;
+using NetWorkshop.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmDB.Repositories
+{
+    public class FilmManager
+    {
+        private ApplicationDbContext _context;
+
+        public FilmManager(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public FilmManager AddFilm(Film film)
+        {
+           
+            _context.Films.Add(film);
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException exp)
+            {
+                Console.WriteLine(exp.Message);
+                film.ID = 0;
+                _context.SaveChanges();
+            }
+
+            return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+            var filmToDelete = GetFilm(id);
+            _context.Films.Remove(filmToDelete);
+            return this;
+        }
+
+        public FilmManager UpdateFilm(Film film)
+        {
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            return this;
+        }
+
+        public Film GetFilm(int id)
+        {
+            return _context.Films.Single(x => x.ID == id);
+        }
+
+        public List<Film> GetFilms()
+        {
+            return null;
+        }
+    }
+}
